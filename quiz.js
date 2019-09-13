d4e4b576afd1d0d4a51def9b0d98eaec12f51a12
AadeshNichite <54974312+AadeshNichite@users.noreply.github.com>
2019-09-13 17:56:32 +0530

    updates

--- quiz.js
+++ quiz.js
@@ -3,14 +3,11 @@ $(function(){
     getJSON();
   
   })
-
-
   function getJSON(){
-    console.log("Starting..");
+   // console.log("Starting..");
    $.getJSON("ques-db.json",function(json){
-       questions = json;
-       //console.log(questions);
-
+    questions = json;
+     //console.log(questions);
     // console.log(Object)
     //    const keys = Object.keys(questions)
     //    console.log(keys.length);
@@ -21,14 +18,21 @@ $(function(){
     // // console.log('hello');
      let set= JSON.parse(localStorage.getItem("jsonn"));
      //console.log(set);
-
     const keys1 = Object.keys(set);
     let q = set[keys1[1]];
      console.log(q["question"]);
-     var k=q["question"]
-     console.log(q["options"]);
+     var key=q["question"];
+    //  var ans=q["options"][0];
+    //  console.log(ans);
+    //  //console.log(k);
+    // console.log(q[options[0]]);
 
-     
+      document.getElementById("que").innerHTML=key;
+
+      document.getElementById("value1").innerHTML=q["options"][0];
+      document.getElementById("value2").innerHTML=q["options"][1];
+      document.getElementById("value3").innerHTML=q["options"][2];
+      document.getElementById("value4").innerHTML=q["options"][3];
 
     // console.log(localStorage.getItem("jsonn"));
    });
