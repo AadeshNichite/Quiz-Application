200590efd568bac9678eb0c78acbc67b523d2f42
AadeshNichite <54974312+AadeshNichite@users.noreply.github.com>
2019-09-13 15:56:01 +0530

    updated

--- quiz.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(".mainPage").show();
-    $(".secondPage").hide();
-    $("#getStart").click(function(){
-        $(".mainPage").hide();
-        $(".secondPage").show();
-    });
-});
