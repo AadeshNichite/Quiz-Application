21a63a7070ecb47e2ac089d494a6384aa223ffbc
AadeshNichite <54974312+AadeshNichite@users.noreply.github.com>
2019-09-14 10:40:58 +0530

    updates

--- quiz.js
+++ quiz.js
@@ -8,8 +8,8 @@ $(function(){
    $.getJSON("ques-db.json",function(json){
     questions = json;
      //console.log(questions);
-     //console.log(Object)
-    const keys = Object.keys(questions)
+     //console.log(Object);
+    const keys = Object.keys(questions);
        // console.log(keys.length);
     //    // [0 -> 1] * 1
     let randIndex = Math.round(Math.random()*(keys.length));
@@ -21,13 +21,20 @@ $(function(){
     const keys1 = Object.keys(set);
     let q = set[keys1[randIndex]];
      console.log(q["question"]);
+
+     
+
+     
      var key=q["question"];
      var answer=q["answer"];
+     sessionStorage.setItem("que","key");
+     sessionStorage.setItem("ans","answer");
     // console.log(answer);
     //  var ans=q["options"][0];
     //  console.log(ans);
     //console.log(key);
     // console.log(q[options[0]]);
+    localStorage.setItem("question",JSON.stringify(q));
       document.getElementById("que").innerHTML=key;
       document.getElementById("value1").innerHTML=q["options"][0];
       document.getElementById("value2").innerHTML=q["options"][1];
@@ -43,6 +50,12 @@ function process()
 {
     var selectedValue=document.getElementById("value1").value;
     console.log(selectedValue);
+    // let set= JSON.parse(localStorage.getItem("jsonn"));
+    // console.log(set);
+    var que = sessionStorage.getItem("que");
+    var ans = sessionStorage.getItem("ans");
+    console.log(question);
+    console.log(answer);
     // if(selectedValue==answer)
     // {
     //     alert("yess");
